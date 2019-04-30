7057303bf14c60461fa3071a0e6e0dcafb364d34
adi.ispas95@gmail.com <adi.ispas95@gmail.com>
2019-05-01 01:23:01 +0300

    Update dataset script

--- kingrec/datasets.py
+++ kingrec/datasets.py
@@ -1,22 +1,22 @@
 import csv
 import pandas as pd
 import numpy as np
+import copy
 from scipy import sparse
 from lightfm.data import Dataset
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MultiLabelBinarizer
+from lightfm.cross_validation import random_train_test_split
 
 __all__ = ['init_movielens']
 
 
-def init_movielens(path, min_rating=0.0):
+def init_movielens(path, min_rating=0.0, k=3):
     train_dataset = Dataset()
     test_dataset = Dataset()
+
     data = dict()
-    cluster_n = 16
+    cluster_n = 14
 
     min_interactions = dict()
-
     with open(path + '/ratings.csv', 'r') as ratings_file:
         reader = csv.reader(ratings_file, delimiter=',', )
         next(reader)  # skip header
@@ -26,72 +26,66 @@ def init_movielens(path, min_rating=0.0):
         items = set()
         for row in reader:
             user_id = int(row[0])
-            users.add(user_id)
-
             item_id = int(row[1])
+
+            users.add(user_id)
             items.add(item_id)
 
             rating = float(row[2])
 
             if rating >= min_rating:
                 ratings.append((user_id, item_id, rating))
+                __add_interaction(min_interactions, user_id)
 
-                if user_id not in min_interactions:
-                    min_interactions.update({user_id: 1})
-                else:
-                    min_interactions.update({user_id: 1 + min_interactions.get(user_id)})
-
-        print('Minimum no of interactions between users and items overall:', min(min_interactions.values()))
+        __info_no_of_min_interactions(k, 'No of interactions per user overall ==> ', min_interactions)
 
         users = list(users)
         items = list(items)
-        ratings_train, ratings_test = train_test_split(ratings, test_size=0.2, random_state=7)
 
-        min_interactions = dict()
-        for user_id, item_id, rating in ratings_train:
-            if user_id not in min_interactions:
-                min_interactions.update({user_id: 1})
-            else:
-                min_interactions.update({user_id: 1 + min_interactions.get(user_id)})
+        users_column, items_column, ratings_column = zip(*ratings)
+        ratings = sparse.coo_matrix((ratings_column, (users_column, items_column)))
 
-        print('Minimum no of interactions between users and items on train:', min(min_interactions.values()))
+        ratings_train, ratings_test = random_train_test_split(ratings, test_percentage=0.2, random_state=np.random.RandomState(7))
 
-        min_interactions = dict()
-        for user_id, item_id, rating in ratings_test:
-            if user_id not in min_interactions:
-                min_interactions.update({user_id: 1})
-            else:
-                min_interactions.update({user_id: 1 + min_interactions.get(user_id)})
+        ratings_train_to_count = zip(ratings_train.row, ratings_train.col, ratings_train.data)
+        ratings_train = zip(ratings_train.row, ratings_train.col, ratings_train.data)
 
-        print('Minimum no of interactions between users and items on test:', min(min_interactions.values()))
+        ratings_test_to_count = zip(ratings_test.row, ratings_test.col, ratings_test.data)
+        ratings_test = zip(ratings_test.row, ratings_test.col, ratings_test.data)
+
+        min_interactions = __count_train_test_min_interactions(ratings_train_to_count)
+        __info_no_of_min_interactions(k, 'No of interactions per user on train ==> ', min_interactions)
+
+        min_interactions = __count_train_test_min_interactions(ratings_test_to_count)
+        __info_no_of_min_interactions(k, 'No of interactions per user on test ==> ', min_interactions)
 
         train_dataset.fit(users=users, items=items)
         test_dataset.fit(users=users, items=items)
 
-        train_interactions = train_dataset.build_interactions(ratings_train)[1]
-        test_interactions = test_dataset.build_interactions(ratings_test)[1]
+        (train_interactions, train_weights) = train_dataset.build_interactions(ratings_train)
+        (test_interactions, test_weights) = test_dataset.build_interactions(ratings_test)
 
         data.update({'train': train_interactions})
         data.update({'test': test_interactions})
 
-    # add genres as features
+    # add genres and posters clusters as features
     movie_genres, genres = __init_movies_genres(path)
-    one_hot = MultiLabelBinarizer()
+    movies_posters_clusters, clusters = __init_movies_posters_clusters(path, cluster_n)
+    aggregated_features = __aggregate_features([movies_posters_clusters])
 
-    movie_genre_features = one_hot.fit_transform(movie_genres.values())
-    movie_genre_features = sparse.coo_matrix(movie_genre_features)
+    train_dataset.fit_partial(item_features=genres)
+    train_dataset.fit_partial(item_features=clusters)
+    train_dataset.fit_partial(items=list(movie_genres.keys()))
+    item_features = train_dataset.build_item_features(((movie_id, aggregated_features.get(movie_id))
+                                                       for movie_id in aggregated_features.keys()))
 
-    # add posters clusters as features
-    movies_posters_clusters = __init_movies_posters_clusters(path, movie_genres.keys(), cluster_n)
-    movies_posters_clusters_features = sparse.coo_matrix(list(movies_posters_clusters.values()))
+    test_dataset.fit_partial(item_features=genres)
+    test_dataset.fit_partial(item_features=clusters)
+    test_dataset.fit_partial(items=list(movie_genres.keys()))
+    _ = test_dataset.build_item_features(((movie_id, aggregated_features.get(movie_id))
+                                          for movie_id in aggregated_features.keys()))
 
-    item_indicator_features = sparse.identity(len(movie_genres))
-    item_features = sparse.hstack([item_indicator_features, movie_genre_features, movies_posters_clusters_features])
     data.update({'item_features': item_features})
-
-    user_indicator_features = sparse.identity(len(users))
-    user_features = sparse.hstack([user_indicator_features])
-    data.update({'user_features': user_features})
 
     return data
 
@@ -114,18 +108,56 @@ def __init_movies_genres(path):
     return movies_genres, genres
 
 
-def __init_movies_posters_clusters(path, movie_ids, cluster_n):
+def __init_movies_posters_clusters(path, cluster_n):
     print('Init movies posters clusters ...')
-    movies_posters_clusters = pd.read_csv(path + '/movies_1_poster_clusters_vgg19.csv')
 
     movie_clusters = dict()
-    for movie_id in movie_ids:
-        movie_clusters[movie_id] = np.zeros(cluster_n, dtype=int).tolist()
+    movies_posters_clusters = pd.read_csv(path + '/movies_1_poster_clusters_vgg19.csv')
 
     for index, _ in movies_posters_clusters.iterrows():
         movie_id = int(movies_posters_clusters['0'][index])
-        poster_cluster = movies_posters_clusters['cluster_' + str(cluster_n)][index]
+        poster_cluster = movies_posters_clusters['cluster_' + str(cluster_n)][index] + 1
 
-        movie_clusters[movie_id][poster_cluster - 1] = 1
+        if movie_id not in movie_clusters:
+            movie_clusters.update({movie_id: ['cluster_' + str(poster_cluster)]})
+        else:
+            movie_clusters.update({movie_id: movie_clusters.get(movie_id).append('cluster_' + str(poster_cluster))})
 
-    return movie_clusters
+    return movie_clusters, ['cluster_' + str(idx + 1) for idx in range(cluster_n)]
+
+
+def __aggregate_features(features_dicts):
+    aggregated_features = dict()
+
+    for features_dict in features_dicts:
+        for item_id in features_dict.keys():
+            if item_id not in aggregated_features:
+                aggregated_features.update({item_id: features_dict.get(item_id)})
+            else:
+                aggregated_features.update({item_id: aggregated_features.get(item_id) + features_dict.get(item_id)})
+
+    return aggregated_features
+
+
+def __count_train_test_min_interactions(ratings_train_to_count):
+    min_interactions = dict()
+    for user_id, item_id, rating in ratings_train_to_count:
+        __add_interaction(min_interactions, user_id)
+
+    return min_interactions
+
+
+def __add_interaction(min_interactions, user_id):
+    if user_id not in min_interactions:
+        min_interactions.update({user_id: 1})
+    else:
+        min_interactions.update({user_id: 1 + min_interactions.get(user_id)})
+
+
+def __info_no_of_min_interactions(k, message, min_interactions):
+    less_k = len([no for no in list(min_interactions.values()) if no < k])
+    total = len(min_interactions.values())
+
+    print(message, 'Min:', min(min_interactions.values()),
+          'Mean:', np.mean(list(min_interactions.values())), 'Max:', max(min_interactions.values()),
+          '<k=' + str(k) + ':', str(less_k) + '/' + str(total))
--- kingrec/kingrec.py
+++ kingrec/kingrec.py
@@ -4,10 +4,12 @@ __all__ = ['KingRec']
 
 
 class KingRec(object):
-    def __init__(self, no_components=50, loss='warp', learning_rate=0.05, alpha=0.02):
+    def __init__(self, no_components=50, loss='warp', learning_rate=0.05, alpha=0.02, scale=0.07):
         self.no_components = no_components
         self.loss = loss
         self.learning_rate = learning_rate
+        self.item_alpha = alpha
+        self.user_alpha = alpha * scale
 
         self.model = LightFM(no_components=no_components, learning_rate=learning_rate,
-                             loss=loss, item_alpha=alpha, user_alpha=alpha, random_state=7)
+                             loss=loss, item_alpha=self.item_alpha, user_alpha=self.user_alpha, random_state=2019)
