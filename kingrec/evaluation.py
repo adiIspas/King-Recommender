bc5e82441548585e4e8fd3a64ef1695eea483b7c
adi.ispas95@gmail.com <adi.ispas95@gmail.com>
2019-05-01 21:56:31 +0300

    Add reciprocal rank in evaluation

--- kingrec/evaluation.py
+++ kingrec/evaluation.py
@@ -2,7 +2,8 @@ from lightfm import evaluation
 
 __all__ = ['precision_at_k',
            'recall_at_k',
-           'auc_score']
+           'auc_score',
+           'reciprocal_rank']
 
 
 def recall_at_k(model, test_interactions, train_interactions=None,
@@ -29,3 +30,11 @@ def auc_score(model, test_interactions, train_interactions=None, user_features=N
     return evaluation.auc_score(model, test_interactions=test_interactions, train_interactions=train_interactions,
                                 user_features=user_features, item_features=item_features, preserve_rows=preserve_rows,
                                 num_threads=num_threads, check_intersections=check_intersections)
+
+
+def reciprocal_rank(model, test_interactions, train_interactions=None, user_features=None, item_features=None,
+                    preserve_rows=False, num_threads=1, check_intersections=True):
+    return evaluation.reciprocal_rank(model, test_interactions=test_interactions, train_interactions=train_interactions,
+                                      user_features=user_features, item_features=item_features,
+                                      preserve_rows=preserve_rows,
+                                      num_threads=num_threads, check_intersections=check_intersections)
